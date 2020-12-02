017d26f30f6ccef00803752a58da8d6f955129e7
Crowdbotics <team@crowdbotics.com>
2020-12-02 17:09:42 +0000

    Update components

--- src/features/BlankScreen015175/screens/blank.js
+++ src/features/BlankScreen015175/screens/blank.js
@@ -27,7 +27,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View>
-      <Text>Update code changes only</Text>
+      <Text>Sample text content</Text>
       <Button title="Press me!" onPress={() => alert("Pressed!")} />
     </View>
   )
