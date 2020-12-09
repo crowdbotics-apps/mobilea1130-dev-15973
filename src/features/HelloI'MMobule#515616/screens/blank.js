dc36e576ea33f0942beead11b98328411ee02cf0
Crowdbotics <team@crowdbotics.com>
2020-12-09 17:48:58 +0000

    add component Hello I'm mobule#

---
+++ src/features/HelloI'MMobule#515616/screens/blank.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+export default class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
+
+  render = () => (
+    <View>
+      <Text>Sample text content</Text>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({ View_1: {}, Text_3: {}, Button_5: {} })
